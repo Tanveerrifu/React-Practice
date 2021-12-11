c56f9d1ea1b63f77602393f0bd69cb868e385aef
Tanveerrifu <tanveerrifu@gmail.com>
2021-12-11 08:10:59 +0600

    final commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+  <div>
+    <nav className="navbar">
+        <img className="logo" src="./logo192.png" alt="" />
+        <h2 className='facts'>React Facts</h2>
+        <h3>React Course Project-1</h3>
+        </nav>
+  </div>
+    )
+}
+
+export default Header
